860268398cd0f4d8f1a1dd5e2fb5d20d1bdc5dff
Guy-Laurent Subri <guy-laurent@subri.ch>
2025-03-11 16:18:34 +0100

    Make about header more mobile friendly

--- website/src/components/AboutHeader.tsx
+++ website/src/components/AboutHeader.tsx
@@ -5,7 +5,7 @@ const DotSeparator = () => <span className="text-gray-300">•</span>;
 
 export const AboutHeader = () => {
   return (
-    <div className="w-full flex flex-row items-center gap-10">
+    <div className="w-full flex flex-col md:flex-row items-center gap-10">
       <img
         src={import.meta.env.BASE_URL + profileImage.src}
         alt="Photo de profile"
@@ -13,7 +13,7 @@ export const AboutHeader = () => {
       />
       <div className="flex flex-col gap-4">
         <H3>À propos de moi</H3>
-        <div className="flex flex-row gap-4 text-gray-600 text-lg">
+        <div className="flex flex-row items-center justify-center gap-4 text-gray-600 text-lg">
           Guy-Laurent Subri
           <DotSeparator />
           31 ans
